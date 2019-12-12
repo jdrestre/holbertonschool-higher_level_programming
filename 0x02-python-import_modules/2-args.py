f67e96b1ff949b8e4c183baf457ed7e326b55d89
jdrestre <jdrestre79@gmail.com>
2019-12-12 23:23:03 +0000

    correction of fixed , spaces and identation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print('0 arguments.')
     elif len(sys.argv) == 2:
-        print('1 argument')
+        print('1 argument:')
         print('1: {:s}'.format(sys.argv[1]))
     else:
         print('{:d} arguments:'.format(len(sys.argv) - 1))
