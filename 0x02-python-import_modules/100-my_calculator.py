8bbc1633aa17eaa94af8fb70778d5dc3d463f070
jdrestre <jdrestre79@gmail.com>
2019-12-14 16:47:47 +0000

    waooo... build my own basic calculator...awsone

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Author: JDRZ
+
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    else:
+        a = sys.argv[1]
+        b = sys.argv[3]
+        if sys.argv[2] == '+':
+            print('{:s} + {:s} = {:d}'.format(a, b, add(int(a), int(b))))
+        elif sys.argv[2] == '-':
+            print('{:s} - {:s} = {:d}'.format(a, b, sub(int(a), int(b))))
+        elif sys.argv[2] == '*':
+            print('{:s} * {:s} = {:d}'.format(a, b, mul(int(a), int(b))))
+        elif sys.argv[2] == '/':
+            print('{:s} / {:s} = {:d}'.format(a, b, div(int(a), int(b))))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            sys.exit(1)
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -10,3 +10,4 @@ File Name|Task Name|Task Description
 3-infinite_add.py | 3. Infinite addition | Write a program that prints the result of the addition of all arguments
 4-hidden_discovery.py | 4. Who are you? | Write a program that prints all the names defined by the compiled module (curl -Lso "hidden_4.pyc") [hidden_4.pyc]("https://github.com/holbertonschool/0x02.py/raw/master/hidden_4.pyc") (please download it locally).
 5-variable_load.py | 5. Everything can be imported | Write a program that imports the variable a from the file variable_load_5.py and prints its value.
+100-my_calculator.py | 6. Build my own calculator! | Write a program that imports all functions from the file calculator_1.py and handles basics operations.
