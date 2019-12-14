76b5c2df8e480155b4fb99c1453f87904ccf232d
jdrestre <jdrestre79@gmail.com>
2019-12-14 17:19:30 +0000

    correction condition > for != into the first request

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) > 4:
+    if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
     else:
