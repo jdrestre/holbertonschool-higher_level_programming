58f37929c02651186da4c2b52902daf33ec36f13
jdrestre <jdrestre79@gmail.com>
2019-12-16 21:19:06 +0000

    fixed, getout import for a funtion call, and add in into loops for

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i range(4, 6):
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
