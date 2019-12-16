929c770b4aeaaff817bbf868a3bb603735ae55ff
jdrestre <jdrestre79@gmail.com>
2019-12-16 13:56:07 +0000

    fix __name__ first to import, change position line

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
     if __name__ == '__main__'
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i range(4, 6):
