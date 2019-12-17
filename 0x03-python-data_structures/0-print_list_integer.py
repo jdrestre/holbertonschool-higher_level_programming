080fdc361f3769d687deff1fefb6967e37b1ed11
jdrestre <jdrestre79@gmail.com>
2019-12-17 21:07:12 +0000

    trying two print options the same

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,3 +2,6 @@
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print('{:d}'.format(my_list[i]))
+    print('----------------')
+    for i in my_list:
+        print('{}'.format(i))
