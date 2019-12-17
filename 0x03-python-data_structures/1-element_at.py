969022c7618be3b78ea9864ca9afc9217c675277
jdrestre <jdrestre79@gmail.com>
2019-12-17 21:51:51 +0000

    first attemp, correction condition , more short

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx > len(my_list):
         return None
-    if idx > len(my_list):
-        return None
-    return(my_list[idx])
+    else:
+        return(my_list[idx])
