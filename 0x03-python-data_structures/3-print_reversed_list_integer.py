39c55450f684098e6e69e1e6bc0e1134f2b07061
jdrestre <jdrestre79@gmail.com>
2019-12-18 02:15:44 +0000

    Considering test cases number one, is there a list?

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return(None)
     reversed = my_list[:]
     reversed.reverse()
     for i in reversed:
