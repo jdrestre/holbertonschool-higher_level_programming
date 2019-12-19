4c3b89d923a486a1454abd6721a7195fcb4f75e3
jdrestre <jdrestre79@gmail.com>
2019-12-19 21:33:19 +0000

    This task can be done in a single line of code.. wtf jejeje

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,3 +5,7 @@ def square_matrix_simple(matrix=[]):
         new_row = list([x**2 for x in row])
         new_matrix.append(new_row)
     return(new_matrix)
+
+'''
+return[[n**2 for n in row] for row in matrix]
+'''
