dc43ffebb0279aee850259264d4c7c496b720683
jdrestre <jdrestre79@gmail.com>
2019-12-20 02:34:13 +0000

    first attemp, correction fixed pep8 - leraning about dictionarys ann keys again, intersting topic and to do operations with the results

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,6 +5,7 @@ def multiply_by_2(a_dictionary):
         new[val] = a_dictionary[val] * 2
     return new
 
+'''
     new_dic = a_dictionary.copy()
     for k, v in new_dic.items():
         new_dic[k] = v * 2
@@ -12,3 +13,4 @@ def multiply_by_2(a_dictionary):
 
 
     return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+'''
