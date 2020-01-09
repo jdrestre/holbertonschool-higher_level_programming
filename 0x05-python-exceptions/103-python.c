27d10bc974ddc792b092fd686bde9c7a56fe1047
jdrestre <jdrestre79@gmail.com>
2020-01-09 17:18:26 +0000

    first function builted for python list

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,44 @@
+#include <Python.h>
+
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - Print basis info Python List
+ * @p: PyObject list object
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	fflush(stdout);
+
+	printf("[*] Python list Info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+
+	printf("[*] Size of th Python list = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			printf_python_bytes(list->ob_item[i]);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(list->ob_item[i]);
+	}
+}
