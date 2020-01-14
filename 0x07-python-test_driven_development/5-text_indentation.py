48e5823a8564a6dfb4b634a476fee8c125a33f52
jdrestre <jdrestre79@gmail.com>
2020-01-14 04:24:25 +0000

    lerning how testint a string with someone special character printed

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
