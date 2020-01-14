c01e8853d4a02a1cfef3d1c1c01a9cff65dfbe61
jdrestre <jdrestre79@gmail.com>
2020-01-14 22:54:48 +0000

    boolean test adding

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,29 @@
 #!/Usr/bin/python3
-"""add_integer function that adds 2 integers
+""" Doc """
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
 """
+def add_integer(a, b=98):
+    """ """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+"""
+
+
+"""add_integer function that adds 2 integers
 
 
 def add_integer(a, b=98):
@@ -13,3 +36,4 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
     return a + b
+"""
