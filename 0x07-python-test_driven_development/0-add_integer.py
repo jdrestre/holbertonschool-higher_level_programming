3fe16148e7e69422f9f5c6ccabd7ac5c79e2f9f1
jdrestre <jdrestre79@gmail.com>
2020-01-14 19:34:46 +0000

    try to resolve the output check NaN float to convert infinity

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,6 @@
 #!/Usr/bin/python3
-""" Doc """
-
-def add_integer(a, b=98):
-    """ Doc """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
-    if a is None or (type(a) is not int and type(a) is not float):
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
-
+"""add_integer function that adds 2 integers
 """
-add_integer function that adds 2 integers
 
 
 def add_integer(a, b=98):
@@ -27,4 +13,3 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
     return a + b
-"""
