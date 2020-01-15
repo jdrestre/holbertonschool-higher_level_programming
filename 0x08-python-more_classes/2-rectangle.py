8f325e5a9e832f5953d756d2b55d28c480be21b0
jdrestre <jdrestre79@gmail.com>
2020-01-15 00:02:38 +0000

    learning about Public instance method

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Define rectangle Class
+"""
+
+
+class Rectangle:
+    """Is a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes rectangle features"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """instance attribute width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """instance attribute height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """property to set it width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """property to set it height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """public instance method that returns rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Public instance method: that returns rectangle perimeter:"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return 2 * (self.__height + self.__width)
