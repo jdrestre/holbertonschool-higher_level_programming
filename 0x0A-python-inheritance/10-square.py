3d727d4594e305a26a1587dcf9978682f2bd0efa
jdrestre <jdrestre79@gmail.com>
2020-01-21 05:20:43 +0000

    change order function super

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """initialize instance size
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
