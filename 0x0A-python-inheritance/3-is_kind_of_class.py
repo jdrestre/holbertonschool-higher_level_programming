a10f30cf8750bd8c3fe9e1680f606d5d10f228c6
jdrestre <jdrestre79@gmail.com>
2020-01-21 00:25:38 +0000

    trying understand inheritance in all level

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""compare object is an instance of a class that inherited 
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
