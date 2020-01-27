d404decc6b28380f341ebb1ae4c45f4342953f78
jdrestre <jdrestre79@gmail.com>
2020-01-27 19:11:53 +0000

    correction test_base and pass command testing of test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 """
-Module for unittests for the Base class
+Module for unittests for the Base class base.py
 """
-import unittest
-import pep8
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 import os
-import json
-
-def test_pep8_conformance_base(self):
-    """Test that we conform to PEP8."""
-    pep8style = pep8.StyleGuide(quiet=True)
-    result = pep8style.check_files(['models/base.py'])
-    self.assertEqual(result.total_errors, 0,
-                     "Found code style errors (and warnings).")
-
+import unittest
+from models.base import Base
 
 
 class TestBaseClassCreation(unittest.TestCase):
