b746f1e2816fdddf017fcaeb3bdbc44502900d29
jdrestre <jdrestre79@gmail.com>
2020-01-28 01:03:23 +0000

    add character # to print Rectangle, i forgot

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,5 +81,5 @@ class Rectangle(Base):
         [print("") for y in range(self.y)]
         for h in range(self.height):
             [print(" ", end="") for x in range(self.x)]
-            [print(" ", end="") for w in range(self.width)]
+            [print("#", end="") for w in range(self.width)]
             print("")
