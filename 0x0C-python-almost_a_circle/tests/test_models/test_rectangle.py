0390da471ba3ca1986bf45cd38aaf2cc9ea86b46
jdrestre <jdrestre79@gmail.com>
2020-01-28 18:53:27 +0000

    remove files test into folder test_models

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines unittests for models/rectangle.py.
-"""
-import io
-import sys
-import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-
