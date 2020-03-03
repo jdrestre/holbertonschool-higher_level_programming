6fa2b71f66460f124dcd0ec18c871f46cb19c4ef
jdrestre <jdrestre79@gmail.com>
2020-03-03 17:47:27 -0500

    learning to create id unique, auto generated and used primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Script that creates database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       PRIMARY KEY(id),
+       id INT NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
+);
