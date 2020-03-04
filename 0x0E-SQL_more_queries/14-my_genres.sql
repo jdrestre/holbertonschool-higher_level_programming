e6d0925232a68e6247ea63f09d7314c0ab687a2f
jdrestre <jdrestre79@gmail.com>
2020-03-04 00:02:17 -0500

    learning how to use double INNER sentence

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT g.`name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
