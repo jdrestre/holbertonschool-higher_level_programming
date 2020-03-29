247919dd7558d6620c8cc6476462c820508e1563
jdrestre <jdrestre79@gmail.com>
2020-03-29 17:51:30 -0500

    understanding and learning the concept SQL injection attack

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# an SQL injection to delete all records of a table…
+# Sintax: ./3-select_states.py username password database_name 'state'
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    [print(state) for state in c.fetchall()]
