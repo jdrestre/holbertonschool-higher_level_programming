8ba855f2056dd66c610a9aa448a7b9df2b4364b0
jdrestre <jdrestre79@gmail.com>
2020-04-09 15:40:28 -0500

    remove all folder prjecto 0x10 Python Network 0

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script takes in a URL, sends a GET request to URL, and displays the body of the response
-curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
