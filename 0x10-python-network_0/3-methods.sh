f39c1cb986e2a3b40b3f8174365a8b97f4cdf12d
jdrestre <jdrestre79@gmail.com>
2020-04-09 17:31:44 -0500

    change filter option awk for cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep Allow | awk '{print $2, $3, $4 }'
+curl -sI "$1" | grep Allow | cut -d" " -f2-
