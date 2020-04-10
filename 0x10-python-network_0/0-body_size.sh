941ed04511bee7b2d022fe33cb7afcf87747d348
jdrestre <jdrestre79@gmail.com>
2020-04-09 19:37:01 -0500

    come back a first solution attemp

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response curl -s "$1" | wc -c
-curl -s -o /dev/null -w "%{size_download}"
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
