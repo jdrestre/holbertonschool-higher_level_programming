3d0d352ce469d1419d510330688652c3e2496676
jdrestre <jdrestre79@gmail.com>
2020-04-15 12:51:32 -0500

    add again task 0, output is ok

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type {}:".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf8")))
