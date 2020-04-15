af9914e0257582d314089e0629131c9e37725bc8
jdrestre <jdrestre79@gmail.com>
2020-04-15 12:29:23 -0500

    I don't know what happend, out it ok like exmple task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    request = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(request) as response:
         html = response.read()
         print('Body response:')
