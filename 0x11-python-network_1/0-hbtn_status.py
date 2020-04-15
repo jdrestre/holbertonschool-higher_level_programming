6571479bc956cfb2b9d54e9a28b76901d257265f
jdrestre <jdrestre79@gmail.com>
2020-04-15 13:27:07 -0500

    yes, ubication : after requeest into content info print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
-        print('\t- content {}:'.format(html))
+        print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf-8')))
