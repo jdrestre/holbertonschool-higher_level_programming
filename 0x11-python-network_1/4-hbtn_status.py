99fbec1c0cdaf18ad611458f4d9014693b9273cb
jdrestre <jdrestre79@gmail.com>
2020-04-15 21:39:50 -0500

    only used requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status.
+Script that fetches https://intranet.hbtn.io/status
+You must use the package requests.
 """
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(request) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html.decode('utf8'))))
-        print('\t- content: {}'.format(html.decode('utf8')))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
