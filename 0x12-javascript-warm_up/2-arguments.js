ed1f0c334bef7d7aae5cf8e300c86d0aae1bfe39
jdrestre <jdrestre79@gmail.com>
2020-05-05 18:37:33 -0500

    Create file to solve task 2 arguments with process.argv.lenth option Update files: README: Update information task .gitignore: update files .vscode

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Script that prints a message depending of the number of arguments passed */
+const arg = process.argv.length;
+if (arg <= 2) {
+  console.log('No argument');
+} if (arg === 3) {
+  console.log('Argument found');
+} if (arg >= 4) {
+  console.log('Arguments found');
+}
+// console.log(arg);
