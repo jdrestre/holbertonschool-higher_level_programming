ed4552b18ca936c2b385fd1f5a2258ae57ec34c8
jdrestre <jdrestre79@gmail.com>
2020-05-05 21:09:24 -0500

    remove definition variables const

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,10 +3,8 @@
 Script that prints **`My number`: `<first argument converted in integer>`**
 if the first argument can be converted to an integer
  */
-const num2 = Number(process.argv[2]);
-const arg2 = process.argv[2];
-if (arg2 && num2) {
-  console.log('My number: ' + arg2);
+if (process.argv[2] && Number(process.argv[2])) {
+  console.log('My number: ' + process.argv[2]);
 } else {
   console.log('Not a number');
 }
