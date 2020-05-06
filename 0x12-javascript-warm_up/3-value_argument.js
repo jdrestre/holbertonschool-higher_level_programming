9d5155443473118b9676c6cdc80c639226e7e6ad
jdrestre <jdrestre79@gmail.com>
2020-05-05 19:16:16 -0500

    Update argv option without length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 /* Script that prints the first argument passed to it */
-const arg = process.argv.length;
 const arg2 = process.argv[2];
-if (arg <= 2) {
-  console.log('No argument');
-} else {
+if (arg2) {
   console.log(arg2);
+} else {
+  console.log('No argument');
 }
