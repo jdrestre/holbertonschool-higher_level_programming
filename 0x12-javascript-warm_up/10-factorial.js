a1b1637224948c2e84a7f17937d10cc92e9b95af
jdrestre <jdrestre79@gmail.com>
2020-05-05 23:14:48 -0500

    Include negative number iqual to 1 factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,10 @@
 /* Script that computes and prints a factorial */
 const argument = Number(process.argv[2]);
 function factorial (num) {
-  if (num === 0 || isNaN(num)) {
+  if (num <= 0 || isNaN(num)) {
     return 1;
   } else {
     return num * factorial(num - 1);
   }
 }
-
 console.log(factorial(argument));
