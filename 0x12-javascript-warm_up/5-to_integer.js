cb4b9130571ec72d954f5e2d4c48c0b038acfd09
jdrestre <jdrestre79@gmail.com>
2020-05-05 21:04:20 -0500

    try to reduce comments

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,8 @@
 #!/user/bin/node
-/* Script that prints **`My number`: `<first argument converted in integer>`** if the first argument can be converted to an integer */
+/*
+Script that prints **`My number`: `<first argument converted in integer>`**
+if the first argument can be converted to an integer
+ */
 const num2 = Number(process.argv[2]);
 const arg2 = process.argv[2];
 if (arg2 && num2) {
